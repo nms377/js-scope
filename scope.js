d3da601003095a2c9efbaf4e67c599d64c3f9a2b
nms377 <nms377@nyu.edu>
2016-11-09 21:28:46 -1000

    Don't really understand why matrix[j].length isn't accepted. Is it because it's not defined yet? I'm guessing you can't us bracket variable in matrix.length because it is not yet defined?

--- scope.js
+++ scope.js
@@ -30,8 +30,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var j = 0; j < matrix.length; j++) {
+      result = result + matrix[i][j];
     }
   }
   return result;
